f3fb7e1e42eb6e78e4f25e843daf53cc9eaf10e6
Chandrakant Singh <chandrakant.singh@syngenta.com>
2021-05-11 23:15:46 +0530

    Removed gtk4 building for webkitgtk

--- filters.py
+++ filters.py
@@ -43,6 +43,9 @@ cmake -DCMAKE_BUILD_TYPE=Release  \\
       -DPORT=GTK                  \\
       -DLIB_INSTALL_DIR=/usr/lib  \\
       -DENABLE_MINIBROWSER=ON     \\
+	  -DENABLE_GLES2=ON           \\
+	  -DENABLE_QUARTZ_TARGET=ON   \\
+	  -DUSE_GTK4=OFF              \\
       -Wno-dev -G Ninja ..        &&
 ninja
 '''
